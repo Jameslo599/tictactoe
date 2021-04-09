2fcee4f93466bf7e70e291b89f6bf68b5eff3890
Jameslo599 <jameslo599@gmail.com>
2021-04-09 00:51:31 -0500

    added all possible victories for crosses and circles

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 const gameObject = (() => {
     const gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    const checkResults = (currentMarker) => {
-        currentMarker = 'X';
-    }
     const _crossVictory = () => {
         if (gameBoard[0] == 'X' && gameBoard[1] == 'X' && gameBoard[2] == 'X') {
+            console.log('You win!');
+        } else if (gameBoard[0] == 'X' && gameBoard[3] == 'X' && gameBoard[6] == 'X') {
+            console.log('You win!');
+        } else if (gameBoard[0] == 'X' && gameBoard[4] == 'X' && gameBoard[8] == 'X') {
+            console.log('You win!');
+        } else if (gameBoard[1] == 'X' && gameBoard[4] == 'X' && gameBoard[7] == 'X') {
+            console.log('You win!');
+        } else if (gameBoard[2] == 'X' && gameBoard[4] == 'X' && gameBoard[6] == 'X') {
+            console.log('You win!');
+        } else if (gameBoard[2] == 'X' && gameBoard[5] == 'X' && gameBoard[8] == 'X') {
+            console.log('You win!');
+        } else if (gameBoard[3] == 'X' && gameBoard[4] == 'X' && gameBoard[5] == 'X') {
+            console.log('You win!');
+        } else if (gameBoard[6] == 'X' && gameBoard[7] == 'X' && gameBoard[8] == 'X') {
+            console.log('You win!');
+        };
+    };
+    const _circleVictory = () => {
+        if (gameBoard[0] == 'O' && gameBoard[1] == 'O' && gameBoard[2] == 'O') {
+            console.log('You win!');
+        } else if (gameBoard[0] == 'O' && gameBoard[3] == 'O' && gameBoard[6] == 'O') {
+            console.log('You win!');
+        } else if (gameBoard[0] == 'O' && gameBoard[4] == 'O' && gameBoard[8] == 'O') {
+            console.log('You win!');
+        } else if (gameBoard[1] == 'O' && gameBoard[4] == 'O' && gameBoard[7] == 'O') {
+            console.log('You win!');
+        } else if (gameBoard[2] == 'O' && gameBoard[4] == 'O' && gameBoard[6] == 'O') {
+            console.log('You win!');
+        } else if (gameBoard[2] == 'O' && gameBoard[5] == 'O' && gameBoard[8] == 'O') {
+            console.log('You win!');
+        } else if (gameBoard[3] == 'O' && gameBoard[4] == 'O' && gameBoard[5] == 'O') {
+            console.log('You win!');
+        } else if (gameBoard[6] == 'O' && gameBoard[7] == 'O' && gameBoard[8] == 'O') {
             console.log('You win!');
         };
     };
@@ -30,6 +60,7 @@ const gameObject = (() => {
             } else {
                 _insertO('topLeft');
                 gameBoard.splice(0, 1, 'O');};
+                _circleVictory();
             marker.removeEventListener('click', check);
         });
     };
@@ -43,6 +74,7 @@ const gameObject = (() => {
             } else {
                 _insertO('topCenter');
                 gameBoard.splice(1, 1, 'O');};
+                _circleVictory();
             marker.removeEventListener('click', check);
         });
     };
@@ -56,6 +88,7 @@ const gameObject = (() => {
             } else {
                 _insertO('topRight');
                 gameBoard.splice(2, 1, 'O');};
+                _circleVictory();
             marker.removeEventListener('click', check);
         });
     };
@@ -65,9 +98,11 @@ const gameObject = (() => {
             if (player1.getMarker() == 'X') {
                 _insertX('centerLeft')
                 gameBoard.splice(3, 1, 'X');
+                _crossVictory();
             } else {
                 _insertO('centerLeft');
                 gameBoard.splice(3, 1, 'O');};
+                _circleVictory();
             marker.removeEventListener('click', check);
         });
     };
@@ -77,9 +112,11 @@ const gameObject = (() => {
             if (player1.getMarker() == 'X') {
                 _insertX('center');
                 gameBoard.splice(4, 1, 'X');
+                _crossVictory();
             } else {
                 _insertO('center');
                 gameBoard.splice(4, 1, 'O');};
+                _circleVictory();
             marker.removeEventListener('click', check);
         });
     };
@@ -89,9 +126,11 @@ const gameObject = (() => {
             if (player1.getMarker() == 'X') {
                 _insertX('centerRight');
                 gameBoard.splice(5, 1, 'X');
+                _crossVictory();
             } else {
                 _insertO('centerRight');
                 gameBoard.splice(5, 1, 'O');};
+                _circleVictory();
             marker.removeEventListener('click', check);
         });
     };
@@ -101,9 +140,11 @@ const gameObject = (() => {
             if (player1.getMarker() == 'X') {
                 _insertX('bottomLeft');
                 gameBoard.splice(6, 1, 'X');
+                _crossVictory();
             } else {
                 _insertO('bottomLeft');
                 gameBoard.splice(6, 1, 'O');};
+                _circleVictory();
             marker.removeEventListener('click', check);
         });
     };
@@ -113,9 +154,11 @@ const gameObject = (() => {
             if (player1.getMarker() == 'X') {
                 _insertX('bottomCenter');
                 gameBoard.splice(7, 1, 'X');
+                _crossVictory();
             } else {
                 _insertO('bottomCenter');
                 gameBoard.splice(7, 1, 'O');};
+                _circleVictory();
             marker.removeEventListener('click', check);
         });
     };
@@ -125,15 +168,17 @@ const gameObject = (() => {
             if (player1.getMarker() == 'X') {
                 _insertX('bottomRight');
                 gameBoard.splice(8, 1, 'X');
+                _crossVictory();
             } else {
                 _insertO('bottomRight');
                 gameBoard.splice(8, 1, 'O');};
+                _circleVictory();
             marker.removeEventListener('click', check);
         });
     };
     return {topLeft, topCenter, topRight,
         centerLeft, center, centerRight,
-    bottomLeft, bottomCenter, bottomRight, gameBoard};
+    bottomLeft, bottomCenter, bottomRight};
 })();
 
 const Player = (name, marker) => {
@@ -142,7 +187,7 @@ const Player = (name, marker) => {
     return {getName, getMarker};
 };
 
-let player1 = Player('james', 'X');
+let player1 = Player('james', 'O');
 
 window.onload = () => {
     gameObject.topLeft();
